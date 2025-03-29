d60e4ab7a5974821ee494010c9012d2c0e455a09
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2025-03-29 12:49:03 -0700

    [feat]: add active peer list

--- lanshare/web_gui/home_page.py
+++ lanshare/web_gui/home_page.py
@@ -1,6 +1,53 @@
 import streamlit as st
+import time
+from streamlit_service import StreamlitService
 
-# Main page content
-st.markdown("# Main page 🎈")
+# Get service instance
 st.sidebar.markdown("# Main page 🎈")
-st.write(f"Username: {st.session_state["username"]}")
+
+# Cache the service instance to keep it from page refresh
+@st.cache_resource
+def setup():
+    return StreamlitService.get_instance(st.session_state.username)
+
+
+
+def main():
+    service = setup()
+
+    st.markdown(f"### Welcome to LAN Share, `{service.username}`!")
+    st.write(
+    "You can interact with others in the local network easily for the following tasks:\n\n"
+    "   👈 **Share files and directories**  \n"
+    "   💬 **Chat with others**  \n"
+    "   📋 **Share clipboard contents**  \n\n"
+    "👈 Open the side bar and start sharing!"
+    )
+
+    st.markdown("---")
+    st.subheader("Online Users")
+
+    active_peers_container = st.empty()
+    
+
+    while True:
+        peers = service.get_active_peers()
+
+        if not peers:
+            active_peers_container.info("No active peers found.")
+        else:
+            data = []
+            for username, peer in peers.items():
+                data.append({
+                    "Username": username,
+                    "IP Address": peer.address,
+                    "Port": str(peer.port),
+                    "Last Seen": peer.last_seen
+                })
+            active_peers_container.dataframe(data)
+
+        time.sleep(1) # refresh every 1 second
+
+
+if __name__ == "__main__":
+    main()
