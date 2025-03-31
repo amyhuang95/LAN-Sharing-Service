f52395e40c09987c5f52526e2046c44f7a339e45
Herry <100013767+healthyxhl23@users.noreply.github.com>
2025-03-30 23:10:26 -0700

    Message_Page Added

--- lanshare/web_gui/streamlit_app.py
+++ lanshare/web_gui/streamlit_app.py
@@ -2,12 +2,19 @@ import streamlit as st
 import sys
 from lanshare.web_gui.service import LANSharingService
 
+st.set_page_config(layout="wide")
+
 
 # Define the pages
 home_page = st.Page("home_page.py", title="Home Page", icon="🌐")
 message_page = st.Page("message_page.py", title="Message", icon="🗣️")
 file_sharing_page = st.Page("file_sharing_page.py", title="Share Files", icon="🤝")
 clipboard_page = st.Page("clipboard_page.py", title="Clipboard", icon="📋")
+
+with st.sidebar:
+    st.markdown("### LAN Share")
+    st.write("LAN Share is an app that makes sharing data over your local network quick and easy!")
+    st.markdown("---")
 
 # Set up navigation
 pg = st.navigation([home_page, message_page, file_sharing_page, clipboard_page])
